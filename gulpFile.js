98540f1c35550e236c2982001a4c03406c60afa0
Seth Krasnianski <sethkrasnianski@gmail.com>
2014-12-14 13:08:58 -0500

    Auto prefixr

--- gulpFile.js
+++ gulpFile.js
@@ -6,7 +6,7 @@ var config      = require('./gulp.json'),
     minCSS      = require('gulp-minify-css'),
     stylus      = require('gulp-stylus'),
     connect     = require('gulp-connect'),
-    aprefix     = require ('autoprefixer-stylus'),
+    aprefix     = require('gulp-autoprefixer'),
     rupture     = require ('rupture');
 
 // Set paths
@@ -51,12 +51,15 @@ gulp.task('stylesheets', function () {
   gulp.src(paths.styles.src)
     .pipe(stylus({
       use: [
-        aprefix(),
         rupture()
       ]
     }))
     // Production
     // .pipe(minCSS())
+   .pipe(aprefix({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
     .pipe(gulp.dest(paths.styles.dest))
     .pipe(connect.reload());
 });
